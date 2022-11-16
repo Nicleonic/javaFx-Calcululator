d057dea7b62c1896ee01e60693e14abcd9176b1e
Nicleonic Johnson <nicleonic2.0@gmail.com>
2022-11-16 15:01:51 +0200

    all right reserved to nic, my first JavaFX App

--- src/main/java/com/example/calctwo/Main.java
+++ src/main/java/com/example/calctwo/Main.java
@@ -1,19 +1,49 @@
 package com.example.calctwo;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+import static com.sun.glass.ui.Window.TRANSPARENT;
+
+public class Main extends Application {
+//    @FXML
+//    private ImageView close;
+//
+//    @FXML
+//    private ImageView minimize;
+    private double x,y;
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Home.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        scene.setFill(Color.TRANSPARENT);
+        stage.setTitle("Calculator");
         stage.setScene(scene);
+        stage.initStyle(StageStyle.TRANSPARENT);
+        stage.setResizable(false);
+
+        // Management of displacement of the window
+        scene.setOnMousePressed(mouseEvent ->{
+                x = mouseEvent.getSceneX();
+                y = mouseEvent.getSceneY();
+        });
+        scene.setOnMouseDragged(mouseEvent->{
+            stage.setX(mouseEvent.getScreenX()-x);
+            stage.setY(mouseEvent.getScreenY()-y);
+        });
+        //===========================================
+
+        ((MainController)fxmlLoader.getController()).init(stage);
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("/0.png")));
         stage.show();
     }
 
--- src/main/java/com/example/calctwo/MainController.java
+++ src/main/java/com/example/calctwo/MainController.java
@@ -1,14 +1,84 @@
 package com.example.calctwo;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
-public class HelloController {
-    @FXML
-    private Label welcomeText;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainController {
+   @FXML
+    private Pane titlePane;
+   @FXML
+   private ImageView close;
+   @FXML
+   private ImageView minimize;
+   @FXML
+   private Label viewer;
+
+    private double num1 = 0;
+    private String operator = "+";
+
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+   void onMouseClicked(MouseEvent event){
+
+   }
+    private  double x,y;
+
+    public void init(Stage stage){
+        titlePane = new Pane();
+        titlePane.setOnMousePressed(mouseEvent ->{
+            x = mouseEvent.getSceneX();
+            y = mouseEvent.getSceneY();
+        });
+        titlePane.setOnMouseDragged(mouseEvent->{
+            stage.setX(mouseEvent.getScreenX()-x);
+            stage.setY(mouseEvent.getScreenY()-y);
+        });
+        close.setOnMouseClicked(mouseEvent -> stage.close());
+        minimize.setOnMouseClicked(mouseEvent -> stage.setIconified(true));
+    }
+
+    @FXML
+    void onNumberClicked(MouseEvent event){
+        //int value = 14;
+        int value = Integer.parseInt(((Label)event.getSource()).getId().replace("ch",""));
+        viewer.setText(Double.parseDouble(viewer.getText())==0?String.valueOf((double)value):String.valueOf(Double.parseDouble(viewer.getText())*10+value));
+    }
+
+    @FXML
+    void onSymbolClicked(MouseEvent event){
+        String symbol = ((Label)event.getSource()).getId().replace("btn","");
+
+        if(symbol.equals("Equals")){
+            double num2 = Double.parseDouble(viewer.getText());
+            switch (operator){
+                case "+" -> viewer.setText((num1+num2) + "") ;
+                case "*" -> viewer.setText((num1*num2) + "") ;
+                case "/" -> viewer.setText((num1/num2) + "") ;
+                case "-" -> viewer.setText((num1-num2) + "") ;
+            }
+            operator = ".";
+        }
+        else if(symbol.equals("Clear")){
+            viewer.setText((String.valueOf(0.0)));
+            operator = ".";
+        }
+        else{
+            switch (symbol){
+                case "Add" -> operator = "+";
+                case "Moins" -> operator = "-";
+                case "Mult" -> operator = "*";
+                case "Div" -> operator = "/";
+            }
+            num1 = Double.parseDouble(viewer.getText());
+            viewer.setText(String.valueOf(0.0));
+        }
     }
 }
